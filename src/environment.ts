dfa376b1b42195af0f9425e59b848ad5b261b6f1
Uteam-TOP <uteam.top.dev@gmail.com>
2025-11-21 15:15:30 +0300

    Update environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    //apiUrl: 'https://uteam.top/api',
     apiUrl: 'https://dev.uteam.top/api',
-    // userNameBot: 'Uteam_top_bot'
     userNameBot: 'Uteam_top_bot'
 };
