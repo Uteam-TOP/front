3b101c6466700238880d3d677d668caad22ad881
unknown <timezero999@gmail.com>
2026-04-13 19:05:34 +0300

    feat: add discussions

---
+++ src/app/pages/discussions/discussions-list/discussions-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discussions-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './discussions-list.component.html',
+  styleUrl: './discussions-list.component.scss'
+})
+export class DiscussionsListComponent {
+
+}
